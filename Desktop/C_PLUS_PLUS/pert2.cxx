92607500944a982b00271588d431ea25edd4226f
Meishella.prog <meishellavy@gmail.com>
2026-09-09 22:51:37 +0700

    pertemuan 2: menulis kode c++ awal, membuat list biodata

---
+++ Desktop/C_PLUS_PLUS/pert2.cxx
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+// int main(){
+//     cout << "Hello kelas 01TPLP013"<< endl;
+//     cout << "Selamat belajar Algoritma dan Pemrograman dasar"<< endl;
+//     return 0;
+// }
+
+int main(){
+     cout << "berikut biodata saya:"<< endl;
+     cout << "Nama: Meishella Noer Alysia" << endl;
+    cout << "Kelas: 01TPLP013" << endl;
+    cout << "Alamat: Jl. Poncol gang II rt.001/rw.004" << endl;
+    cout << "Tempat, Tanggal Lahir: Jakarta, 19 Mei 2007" << endl;
+    return 0;
+}
