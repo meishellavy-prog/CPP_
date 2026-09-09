d9273d1bc0df6f9d87130a13bbe76a2b267b84e8
Meishella.prog <meishellavy@gmail.com>
2026-09-09 23:21:09 +0700

    penggabungan

--- Desktop/C_PLUS_PLUS/pert2.cxx
+++ Desktop/C_PLUS_PLUS/pert2.cxx
@@ -3,14 +3,13 @@ using namespace std;
 int main(){
     cout << "Hello kelas 01TPLP013"<< endl;
     cout << "Selamat belajar Algoritma dan Pemrograman dasar"<< endl;
-    return 0;
-}
-
-int main(){
-     cout << "berikut biodata saya:"<< endl;
-     cout << "Nama: Meishella Noer Alysia" << endl;
+    //BIODATA:
+    cout << "berikut biodata saya:"<< endl;
+    cout << "Nama: Meishella Noer Alysia" << endl;
     cout << "Kelas: 01TPLP013" << endl;
     cout << "Alamat: Jl. Poncol gang II rt.001/rw.004" << endl;
     cout << "Tempat, Tanggal Lahir: Jakarta, 19 Mei 2007" << endl;
     return 0;
 }
+
+
